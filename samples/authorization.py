7c67f2b70a2157fc747d582114287639293a895b
roger778876 <rogercesar778876@gmail.com>
2019-05-04 01:52:24 -0400

    basic_functions sample file

--- samples/authorization.py
+++ samples/authorization.py
@@ -9,18 +9,18 @@ scope = 'user-library-read'
 
 
 if len(sys.argv) > 1:
-    username = sys.argv[1]
+  username = sys.argv[1]
 else:
-    print "Usage: %s username" % (sys.argv[0],)
-    sys.exit()
+  print "Usage: %s username" % (sys.argv[0],)
+  sys.exit()
 
 token = util.prompt_for_user_token(username, scope) #, clientID, clientSecret, redirectURL)
 
 if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print track['name'] + ' - ' + track['artists'][0]['name']
+  sp = spotipy.Spotify(auth=token)
+  results = sp.current_user_saved_tracks()
+  for item in results['items']:
+    track = item['track']
+    print track['name'] + ' - ' + track['artists'][0]['name']
 else:
-    print "Can't get token for", username
+  print "Can't get token for", username
---
+++ samples/basic_functions.py
@@ -0,0 +1,33 @@
+import sys
+import spotipy
+import spotipy.util as util
+
+scope = 'user-read-currently-playing user-read-playback-state'
+
+def curr_song(sp):
+  print sp.currently_playing()["item"]["name"]
+
+def devices(sp):
+  for device in sp.devices()["devices"]:
+    print device["name"]
+
+def main():
+  if len(sys.argv) > 1:
+    username = sys.argv[1]
+  else:
+    print "Usage: %s username" % (sys.argv[0],)
+    sys.exit()
+
+  token = util.prompt_for_user_token(username, scope) #, clientID, clientSecret, redirectURL)
+
+  if token:
+    sp = spotipy.Spotify(auth=token)
+    curr_song(sp)
+    devices(sp)
+
+  else:
+    print "Can't get token for", username
+
+
+
+main()
