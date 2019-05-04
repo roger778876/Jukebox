618fa9d12b442ce0ed168d527786e97d0c90f9e2
roger778876 <rogercesar778876@gmail.com>
2019-05-03 22:57:40 -0400

    added authorization to samples

---
+++ samples/authorization.py
@@ -0,0 +1,26 @@
+import sys
+import spotipy
+import spotipy.util as util
+
+scope = 'user-library-read'
+# clientID = ''
+# clientSecret = ''
+# redirectURL = 'http://localhost/'  # We/Spotify suggest http://localhost:8888/callback/ or http://localhost/
+
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print "Usage: %s username" % (sys.argv[0],)
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope) #, clientID, clientSecret, redirectURL)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print track['name'] + ' - ' + track['artists'][0]['name']
+else:
+    print "Can't get token for", username
